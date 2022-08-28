a44237f542c1979568e58e8367d41dd052039262
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-08-28 16:11:18 -0400

    Update Hero.tsx

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
         <div className="relative pb-16 sm:pb-24 ">
           <main className="mx-auto sm:px-4 tracking-wider text-black ">
             <div className="text-center flex flex-col justify-between h-full xs:px-6 sm:px-0">
-          <h1 className="font-extrabold text-7xl md:text-8xl mferFont tracking-wider flex gap-4 justify-center">
+          <h1 className="title font-extrabold text-7xl md:text-8xl mferFont tracking-wider flex gap-4 justify-center">
                 The Candy Shop
               </h1>
           <h4></h4>
