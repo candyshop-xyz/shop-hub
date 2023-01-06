67d84099e2d7e2a766ebcc2083901e70bd4a9e4c
thebadcc <95499220+thebadcc@users.noreply.github.com>
2023-01-05 19:41:59 -0500

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,7 +15,7 @@ const Custom404 = () => {
       </PageHeader>
 
       <PageContent>
-        <img className="border-2 mt-3 rounded-xl p-2 bg-white shadow-md" src="404.png" alt="error" />
+        <img className="" src="404.png" alt="error" />
       </PageContent>
     </>
   );
